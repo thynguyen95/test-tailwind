f64365fe40ef57e5b2bd58becfc8e68129f2e29c
ThyNguyen <thynguyen.tn293@gmail.com>
2024-10-09 00:57:25 +0700

    up src

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translate(0)" },
+          "100%": { transform: "translate(calc(-100% - 2.5rem))" },
+        },
+      },
+      animation: {
+        marquee: "marquee 50s linear infinite",
+      },
+      backgroundImage: {
+        main: "linear-gradient(270.78deg, #c3dafe 4.19%, #e9d8fd 96.11%)",
+      },
+      width: {
+        w: "50",
+      },
+      height: {
+        86: "350px",
+      },
+      translate: {
+        700: "700px",
+      },
+      lineHeight: {
+        16: "64px",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
